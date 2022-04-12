572fd1e32fbb04df962e7fae6d2407dbb42deba2
achilleswargod <65618715+achilleswargod@users.noreply.github.com>
2022-04-12 14:53:40 -0500

    Imported Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
-  return <div></div>;
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  });
+
+  async function getAllSongs() {
+    let response = await axios.get("http://127.0.0.1:8000/api/musiclibrary/");
+    setSongs(response.data);
+    console.log(response.data);
+  }
+
+  return (
+    <div>
+      <h1>Async Await Axios Example</h1>
+    </div>
+  );
 }
 
 export default App;
